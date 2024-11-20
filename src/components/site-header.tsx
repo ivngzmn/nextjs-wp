9ac7039e8ee4fca1decf2907e704576392370044
Ivan Guzman <ivanguzmandev@gmail.com>
2024-11-19 16:59:58 -0800

    cleanup unused imports

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
 
 export function SiteHeader() {
